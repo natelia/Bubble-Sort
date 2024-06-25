c8f3257acd5dfe550ea3ce919a8285cf0b9c6059
Natalia <natalia.jadwiga.gajewska@gmail.com>
2024-06-25 14:33:39 +0200

    Refactored code for readability and style compliance

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,20 +1,16 @@
-
+# frozen_string_literal: true
 
 def bubble_sort(arr)
   n = arr.length
 
   (0...n).each do |i|
     (0...(n - 1)).each do |m|
-
-      if arr[m] > arr[m + 1]
-        arr[m], arr[m + 1] = arr[m + 1], arr[m] 
-      end
+      arr[m], arr[m + 1] = arr[m + 1], arr[m] if arr[m] > arr[m + 1]
     end
   end
-
 
   arr
 end
 
-sorted_array = bubble_sort([4,3,78,2,0,2])
+sorted_array = bubble_sort([4, 3, 78, 2, 0, 2])
 puts sorted_array
